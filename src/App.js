e32c062c465dfeef16ee3384ec3a834a60947cf6
superbkibbles <mend_mzury@hotmail.com>
2021-01-04 00:20:47 +0300

    last touch

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { interval } from 'rxjs';
 
 import Timer from './components/timer';
 import AppButton from './components/appButton';
 import './App.css';
 
-//testing
-import { interval } from 'rxjs';
-import { share } from 'rxjs/operators';
 
 
-const sec$ = interval(1000).pipe(
-    share()
-)
+const sec$ = interval(1000);
 
 const App = () => {
     const [seconds, setSeconds] = useState(0);
     const [play, setPlay] = useState(false);
+    const [waiting, setWaiting] = useState(false);
 
     useEffect(() => {
         let subscription = sec$.subscribe(() => {
-            if (play) {
+            if (play && !waiting) {
                 setSeconds(seconds + 1);
             }
         });
 
         return () => subscription.unsubscribe();
-    }, [seconds, play])
+    }, [seconds, play, waiting])
 
 
 
@@ -37,6 +34,13 @@ const App = () => {
         setPlay(!play)
     }
 
+    const wait = () => {
+        setWaiting(true);
+        setTimeout(() => {
+            setWaiting(false)
+        }, 1000)
+    }
+
     return (
         <div className="App">
             <div className='App_header'>
@@ -46,13 +50,24 @@ const App = () => {
                 seconds={ seconds }
             />
             <div className='btn-container'>
-                <AppButton
-                    clickHandler={ reset }
-                    name='Reset'
-                    style={ play || seconds < 1 ?
-                    { opacity: .2, pointerEvents: "none" }:
-                    {}}
-                />
+                {
+                    !play && !waiting?
+                        <AppButton
+                            clickHandler={ reset }
+                            name='Reset'
+                            style={ play || seconds < 1 ?
+                                { opacity: .2, pointerEvents: "none" }:
+                                {}}
+                        />:
+                        <AppButton
+                            clickHandler={ wait }
+                            name='Wait'
+                            style={
+                                waiting? { opacity: .2, pointerEvents: "none" }:
+                            { background: '#2828e0' }}
+                        />
+                }
+
                 { !play ?
                     <AppButton
                         clickHandler={ stopOrPlay }
